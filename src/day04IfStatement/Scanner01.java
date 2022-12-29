ae934008931cf5f882b674ce5e5f849d859079e3
Fatih Turker <fthturker80@gmail.com>
2022-12-29 21:17:06 +0300

    29.12.22

--- src/day04IfStatement/Scanner01.java
+++ src/day04IfStatement/Scanner01.java
@@ -10,7 +10,10 @@ public class Scanner01 {
         Ali Mert Can
         123456789
          */
+        // 1. Yol
+
         Scanner input = new Scanner(System.in);
+        /*
         System.out.println("Ilk isminizi giriniz:");
         String ilkIsim = input.next(); // next() methodu kullanicidan string datasi almak icin kullanilir
 
@@ -25,6 +28,32 @@ public class Scanner01 {
 
         System.out.println(ilkIsim + " " + ortaIsim + " " + soyIsim);
         System.out.println(kimlikNo);
+
+        // 2. Yol
+        System.out.println("Ilk, orta, soy isminizi ve kimlik numaranizi giriniz...");
+        String ilk=input.next();
+        String orta=input.next();
+        String soy=input.next();
+        String kimlik=input.next();
+
+        System.out.println(ilk+" "+orta+" "+" "+soy);
+        System.out.println(kimlik);
+         */
+        /*
+        next() ile nextLine() in farki nedir?
+        nextLine() ==> kullanicidan String in tamamini alir
+        next() ==> kullanicidan gelen String in sadece ilk kelimesini alir
+         */
+        // 3. Yol
+        System.out.println("Ilk, orta, soy isminizi giriniz...");
+        String tamIsim=input.nextLine();
+        System.out.println(tamIsim);
+
+        System.out.println("kimlik numaranizi giriniz: ");
+        String kimlikNumarasi=input.next();
+        System.out.println(kimlikNumarasi);
+
+
 
     }
 }
